f76d5b018ea0cce9eb1c2f182438b91539079f39
Franz Pletz <fpletz@fnordicwalking.de>
2025-03-18 10:28:29 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'crane':
        'github:ipetkov/crane/5245473d6638a96da540e44372da96eebb97735a' (2025-03-03)
      → 'github:ipetkov/crane/de3bb0155823298161c1c0a7805f10d4b4074bbb' (2025-03-16)
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/3876f6b87db82f33775b1ef5ea343986105db764' (2025-03-01)
      → 'github:hercules-ci/flake-parts/f4330d22f1c5d2ba72d3d22df5597d123fdb60a9' (2025-03-07)
    • Updated input 'flake-parts/nixpkgs-lib':
        'https://github.com/NixOS/nixpkgs/archive/6d3702243441165a03f699f64416f635220f4f15.tar.gz?narHash=sha256-3wHafybyRfpUCLoE8M%2BuPVZinImg3xX%2BNm6gEfN3G8I%3D' (2025-03-01)
      → 'github:nix-community/nixpkgs.lib/147dee35aab2193b174e4c0868bd80ead5ce755c' (2025-03-02)
    • Updated input 'nix-fast-build':
        'github:Mic92/nix-fast-build/906af17fcd50c84615a4660d9c08cf89c01cef7d' (2025-01-11)
      → 'github:Mic92/nix-fast-build/0ef0c6065345426fb4872c9ee75a58c11f62d3f8' (2025-03-13)
    • Updated input 'nix-fast-build/treefmt-nix':
        'github:numtide/treefmt-nix/13c913f5deb3a5c08bb810efd89dc8cb24dd968b' (2025-01-06)
      → 'github:numtide/treefmt-nix/3d0579f5cc93436052d94b73925b48973a104204' (2025-02-17)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/ba487dbc9d04e0634c64e3b1f0d25839a0a68246' (2025-03-03)
      → 'github:NixOS/nixpkgs/c80f6a7e10b39afcc1894e02ef785b1ad0b0d7e5' (2025-03-15)
    • Updated input 'pre-commit-hooks':
        'github:cachix/pre-commit-hooks.nix/42b1ba089d2034d910566bf6b40830af6b8ec732' (2025-03-02)
      → 'github:cachix/pre-commit-hooks.nix/59f17850021620cd348ad2e9c0c64f4e6325ce2a' (2025-03-15)

--- flake.nix
+++ flake.nix
@@ -13,7 +13,6 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixpkgs";
       };
     };
     nix-fast-build = {
